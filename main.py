2bb93291e288692317cc7b96e4c17d82e18dfe05
l-palette <l-palette>
2025-04-03 16:03:13 +0500

    Update README.md and fix main.py

--- main.py
+++ main.py
@@ -1,24 +1,62 @@
 from datetime import datetime
 from typing import List, Optional
-from validate import validate_name, validate_authors, validate_genres, validate_year, validate_width, validate_height, validate_book_type, validate_source, validate_date_added, validate_date_read, validate_rating, validate_search_field, validate_update_name, validate_update_field
+from validate import (
+    validate_name,
+    validate_authors,
+    validate_genres,
+    validate_year,
+    validate_width,
+    validate_height,
+    validate_book_type,
+    validate_source,
+    validate_date_added,
+    validate_date_read,
+    validate_rating,
+    validate_search_field,
+    validate_update_name,
+    validate_update_field,
+)
 from validate import validation_functions, db_schema
 
-FILENAME = 'home_library.txt'
-HEADERS = ['name', 'year', 'authors', 'genres', 'width', 'height', 'book_type', 'source', 'date_added', 'date_read', 'rating']
+FILENAME = "home_library.txt"
+HEADERS = [
+    "name",
+    "year",
+    "authors",
+    "genres",
+    "width",
+    "height",
+    "book_type",
+    "source",
+    "date_added",
+    "date_read",
+    "rating",
+]
+
 
 class Book:
     _last_id = 0
 
-    def __init__(self, name: str, year: int, width: float, height: float, 
-                 book_type: str, source: str, 
-                 date_added: str, date_read: Optional[str], rating: str, 
-                 authors: List[str], genres: List[str]) -> None:
-        
+    def __init__(
+        self,
+        name: str,
+        year: int,
+        width: float,
+        height: float,
+        book_type: str,
+        source: str,
+        date_added: str,
+        date_read: Optional[str],
+        rating: str,
+        authors: List[str],
+        genres: List[str],
+    ) -> None:
+
         Book._last_id += 1
         self.id = Book._last_id
         self.name = name
-        self.authors = ','.join(authors)
-        self.genres = ','.join(genres) 
+        self.authors = ",".join(authors)
+        self.genres = ",".join(genres)
         self.year = year
         self.width = width
         self.height = height
@@ -43,6 +81,7 @@ class Book:
         validate_date_read(self.date_read, self.date_added)
         validate_rating(self.rating)
 
+
 class HomeLibrary:
     def __init__(self):
         self.books = []
@@ -53,9 +92,11 @@ class HomeLibrary:
 
     def select_books(self) -> List[str]:
         return [book.name for book in self.books]
-    
+
     def search_books(self, field: str, value: str) -> List[str]:
-        return [book.name for book in self.books if value in str(getattr(book, field, ''))]
+        return [
+            book.name for book in self.books if value in str(getattr(book, field, ""))
+        ]
 
     def delete_book(self, name: str) -> None:
         self.books = [book for book in self.books if book.name != name]
@@ -72,11 +113,23 @@ class HomeLibrary:
 
     def load_books_from_file(self):
         try:
-            with open(FILENAME, 'r', encoding='utf-8') as file:
+            with open(FILENAME, "r", encoding="utf-8") as file:
                 for line in file:
-                    data = line.strip().split('|')
+                    data = line.strip().split("|")
                     if len(data) == len(HEADERS):
-                        name, year, authors, genres, width, height, book_type, source, date_added, date_read, rating = data
+                        (
+                            name,
+                            year,
+                            authors,
+                            genres,
+                            width,
+                            height,
+                            book_type,
+                            source,
+                            date_added,
+                            date_read,
+                            rating,
+                        ) = data
                         book = Book(
                             name=name,
                             authors=authors.split(","),
@@ -97,15 +150,17 @@ class HomeLibrary:
             print(f"Ошибка при загрузке книг: {e}")
 
     def save_books_to_file(self):
-        with open(FILENAME, 'w', encoding='utf-8') as file:
+        with open(FILENAME, "w", encoding="utf-8") as file:
             for book in self.books:
-                file.write(f"{book.name}|{book.year}|{book.authors}|{book.genres}|{book.width}|{book.height}|{book.book_type}|{book.source}|{book.date_added}|{book.date_read}|{book.rating}\n")
+                file.write(
+                    f"{book.name}|{book.year}|{book.authors}|{book.genres}|{book.width}|{book.height}|{book.book_type}|{book.source}|{book.date_added}|{book.date_read}|{book.rating}\n"
+                )
+
 
 def get_input(prompt, field, validation_method, extra_field=None):
     while True:
         value = input(prompt).strip()
         try:
-            value = make_correct_type(value, db_schema[field]['type'])
             if validation_method:
                 if extra_field:
                     validation_method(value, extra_field)
@@ -114,6 +169,7 @@ def get_input(prompt, field, validation_method, extra_field=None):
             return value
         except (ValueError, TypeError) as e:
             print(f"Ошибка: {e}")
+
 
 def display_menu():
     print("\n--- Меню Библиотеки ---")
@@ -124,31 +180,70 @@ def display_menu():
     print("5. Удалить книгу")
     print("6. Выход")
 
+
 def make_correct_type(value, expected_type):
     if not isinstance(value, expected_type):
         value = expected_type(value)
     return value
 
+
 def main():
     library = HomeLibrary()
-    
     while True:
         display_menu()
         choice = input("Выберите опцию (1-6): ")
 
-        if choice == '1':
+        if choice == "1":
             try:
-                name = get_input("Введите название книги: ", 'name', validate_name)
-                authors = get_input("Введите авторов (через запятую): ", 'authors', validate_authors)
-                genres = get_input("Введите жанры (через запятую): ", 'genres', validate_genres)
-                year = int(get_input("Введите год издания: ", 'year', validate_year))
-                width = float(get_input("Введите ширину книги (в мм, например 200.0): ", 'width', validate_width))
-                height = float(get_input("Введите высоту книги (в мм, например 200.0): ", 'height', validate_height))
-                book_type = get_input("Введите тип книги (мягкий/твердый): ", 'book_type', validate_book_type)
-                source = get_input("Введите источник (покупка/подарок/наследство): ", 'source', validate_source)
-                date_added = get_input("Введите дату добавления (дд-мм-гггг): ", 'date_added', validate_date_added, year)
-                date_read = get_input("Введите дату чтения (дд-мм-гггг, оставьте пустым, если не читали): ", 'date_read', validate_date_read, date_added)
-                rating = get_input("Введите оценку книги (n/10 - комментарий, оставьте пустым, если не читали): ", 'rating', validate_rating)
+                name = get_input("Введите название книги: ", "name", validate_name)
+                authors = get_input(
+                    "Введите авторов (через запятую): ", "authors", validate_authors
+                )
+                genres = get_input(
+                    "Введите жанры (через запятую): ", "genres", validate_genres
+                )
+                year = int(get_input("Введите год издания: ", "year", validate_year))
+                width = float(
+                    get_input(
+                        "Введите ширину книги (в мм, например 200.0): ",
+                        "width",
+                        validate_width,
+                    )
+                )
+                height = float(
+                    get_input(
+                        "Введите высоту книги (в мм, например 200.0): ",
+                        "height",
+                        validate_height,
+                    )
+                )
+                book_type = get_input(
+                    "Введите тип книги (мягкий/твердый): ",
+                    "book_type",
+                    validate_book_type,
+                )
+                source = get_input(
+                    "Введите источник (покупка/подарок/наследство): ",
+                    "source",
+                    validate_source,
+                )
+                date_added = get_input(
+                    "Введите дату добавления (дд-мм-гггг): ",
+                    "date_added",
+                    validate_date_added,
+                    year,
+                )
+                date_read = get_input(
+                    "Введите дату чтения (дд-мм-гггг, оставьте пустым, если не читали): ",
+                    "date_read",
+                    validate_date_read,
+                    date_added,
+                )
+                rating = get_input(
+                    "Введите оценку книги (n/10 - комментарий, оставьте пустым, если не читали): ",
+                    "rating",
+                    validate_rating,
+                )
 
                 book = Book(
                     name=name,
@@ -169,33 +264,56 @@ def main():
             except (ValueError, TypeError) as e:
                 print("Ошибка при добавлении книги:", e)
 
-        elif choice == '2':
+        elif choice == "2":
             print("Книги в библиотеке:", library.select_books())
 
-        elif choice == '3':
-            field = get_input("Введите поле для поиска (name/authors/genres): ", 'search_field', validate_search_field)
-            value = get_input("Введите значение для поиска: ", field, validation_functions[field])
+        elif choice == "3":
+            field = get_input(
+                "Введите поле для поиска (name/authors/genres): ",
+                "search_field",
+                validate_search_field,
+            )
+            value = get_input(
+                "Введите значение для поиска: ", field, validation_functions[field]
+            )
             results = library.search_books(field, value)
             print("Результаты поиска:", results)
 
-        elif choice == '4':
-            name = get_input("Введите название книги для обновления: ", 'update_name', validate_update_name, library)
-            field = get_input("Введите поле для обновления (name/authors/genres/year/width/height/book_type/source/date_added/date_read/rating):", 'update_field', validate_update_field)
-            new_value = get_input("Введите новое значение: ", field, validation_functions[field])
+        elif choice == "4":
+            name = get_input(
+                "Введите название книги для обновления: ",
+                "update_name",
+                validate_update_name,
+                library,
+            )
+            field = get_input(
+                "Введите поле для обновления (name/authors/genres/year/width/height/book_type/source/date_added/date_read/rating):",
+                "update_field",
+                validate_update_field,
+            )
+            new_value = get_input(
+                "Введите новое значение: ", field, validation_functions[field]
+            )
             library.update_book(name, field, new_value)
 
-        elif choice == '5':
-            name = get_input("Введите название книги для удаления: ", 'name', validate_update_name, library)
+        elif choice == "5":
+            name = get_input(
+                "Введите название книги для удаления: ",
+                "name",
+                validate_update_name,
+                library,
+            )
             library.delete_book(name)
             print("Книга удалена.")
 
-        elif choice == '6':
+        elif choice == "6":
             library.save_books_to_file()  # Сохраняем книги в файл перед выходом
             print("Выход из программы.")
             break
 
         else:
             print("Неверный выбор. Пожалуйста, выберите опцию от 1 до 6.")
+
 
 if __name__ == "__main__":
     main()
--- validate.py
+++ validate.py
@@ -4,108 +4,127 @@ from typing import Optional, List, Literal
 
 # Define the schema for validation
 db_schema = {
-    'name': {'type': str, 'length': 51, 'regex': r'[А-Яа-яЁёA-Za-z0-9\s]+'},
-    'authors': {'type': str, 'length': 51, 'regex': r'[А-Яа-яЁёA-Za-z\s,]+'},
-    'genres': {'type': str, 'length': 71, 'regex': r'[А-Яа-яЁёA-Za-z\s,]+'},       
-    'year': {'type': int, 'length': 5, 'regex': r'\d{4}'},
-    'width': {'type': float, 'length': 6, 'regex': r'\d+\.\d+'},
-    'height': {'type': float, 'length': 6, 'regex': r'\d+\.\d+'},
-    'book_type': {'type': str, 'length': 8, 'regex': r'мягкий|твердый'},
-    'source': {'type': str, 'length': 11, 'regex': r'покупка|подарок|наследство'}, 
-    'date_added': {'type': str, 'length': 11, 'regex': r'\d{2}-\d{2}-\d{4}'}, 
-    'date_read': {'type': Optional[str], 'length': 11, 'regex': r'\d{2}-\d{2}-\d{4}'}, 
-    'rating': {'type': Optional[str], 'length': 101, 'regex': r'([1-9]|10)/10 - .'},
-    'search_field': {'type': str, 'length': 1000, 'regex': r'authors|genres|name'},
-    'update_name': {'type': str, 'length': 1000, 'regex': r'.*'},
-    'update_field': {'type': str, 'length': 1000, 'regex': r'name|year|authors|genres|width|height|book_type|source|date_added|date_read|rating'},
+    "name": {"type": str, "length": 51, "regex": r"[А-Яа-яЁёA-Za-z0-9\s]+"},
+    "authors": {"type": str, "length": 51, "regex": r"[А-Яа-яЁёA-Za-z\s,]+"},
+    "genres": {"type": str, "length": 71, "regex": r"[А-Яа-яЁёA-Za-z\s,]+"},
+    "year": {"type": int, "length": 5, "regex": r"\d{4}"},
+    "width": {"type": float, "length": 6, "regex": r"\d+\.\d+"},
+    "height": {"type": float, "length": 6, "regex": r"\d+\.\d+"},
+    "book_type": {"type": str, "length": 8, "regex": r"мягкий|твердый"},
+    "source": {"type": str, "length": 11, "regex": r"покупка|подарок|наследство"},
+    "date_added": {"type": str, "length": 11, "regex": r"\d{2}-\d{2}-\d{4}"},
+    "date_read": {"type": Optional[str], "length": 11, "regex": r"\d{2}-\d{2}-\d{4}"},
+    "rating": {"type": Optional[str], "length": 101, "regex": r"([1-9]|10)/10 - ."},
+    "search_field": {"type": str, "length": 1000, "regex": r"authors|genres|name"},
+    "update_name": {"type": str, "length": 1000, "regex": r".*"},
+    "update_field": {
+        "type": str,
+        "length": 1000,
+        "regex": r"name|year|authors|genres|width|height|book_type|source|date_added|date_read|rating",
+    },
 }
+
 
 # Validation functions
 def validate_field(field: str, value) -> None:
-    if len(str(value)) > db_schema[field]['length']:
-        raise ValueError(f'Переменная {field} превышает лимит символов')
-    
-    if not re.match(db_schema[field]['regex'], str(value)):
-        raise ValueError(f'Неверное значение для {field}: {value}')
+    if len(str(value)) > db_schema[field]["length"]:
+        raise ValueError(f"Переменная {field} превышает лимит символов")
+    if not re.match(db_schema[field]["regex"], str(value)):
+        raise ValueError(f"Неверное значение для {field}: {value}")
+
 
 def validate_name(name) -> None:
-    validate_field('name', name)
+    validate_field("name", name)
+
 
 def validate_authors(authors) -> None:
-    validate_field('authors', authors)
+    validate_field("authors", authors)
+
 
 def validate_genres(genres) -> None:
-    validate_field('genres', genres)
+    validate_field("genres", genres)
+
 
 def validate_year(year) -> None:
-    validate_field('year', year)
+    validate_field("year", year)
     if int(year) > datetime.now().year:
-        raise ValueError('Год издания не может быть больше текущего года')
+        raise ValueError("Год издания не может быть больше текущего года")
+
 
 def validate_width(width) -> None:
-    validate_field('width', width)
+    validate_field("width", width)
     if float(width) > 1000:
-        raise ValueError('Ширина книги не может быть больше 1 метра')
+        raise ValueError("Ширина книги не может быть больше 1 метра")
     elif float(width) < 0:
-        raise ValueError('Ширина книги не может быть отрицательной')
+        raise ValueError("Ширина книги не может быть отрицательной")
+
 
 def validate_height(height) -> None:
-    validate_field('height', height)
+    validate_field("height", height)
     if float(height) > 1000:
-        raise ValueError('Высота книги не может быть больше 1 метра')
+        raise ValueError("Высота книги не может быть больше 1 метра")
     elif float(height) < 0:
-        raise ValueError('Высота книги не может быть отрицательной')
+        raise ValueError("Высота книги не может быть отрицательной")
+
 
 def validate_book_type(book_type) -> None:
-    validate_field('book_type', book_type)
+    validate_field("book_type", book_type)
+
 
 def validate_source(source) -> None:
-    validate_field('source', source)
+    validate_field("source", source)
+
 
 def validate_date_added(date_added, year) -> None:
-    validate_field('date_added', date_added)
-    date_added_datetime = datetime.strptime(date_added, '%d-%m-%Y')
+    validate_field("date_added", date_added)
+    date_added_datetime = datetime.strptime(date_added, "%d-%m-%Y")
     if date_added_datetime >= datetime.now():
-        raise ValueError('Дата добавления не может быть позже текущей даты')
+        raise ValueError("Дата добавления не может быть позже текущей даты")
     if date_added_datetime.year < year:
-        raise ValueError('Год добавления не может быть раньше года издания')
+        raise ValueError("Год добавления не может быть раньше года издания")
+
 
 def validate_date_read(date_read, date_added) -> None:
     if date_read:
-        validate_field('date_read', date_read)
-        date_added_datetime = datetime.strptime(date_added, '%d-%m-%Y')
-        date_read_datetime = datetime.strptime(date_read, '%d-%m-%Y')
+        validate_field("date_read", date_read)
+        date_added_datetime = datetime.strptime(date_added, "%d-%m-%Y")
+        date_read_datetime = datetime.strptime(date_read, "%d-%m-%Y")
         if date_read_datetime < date_added_datetime:
-            raise ValueError('Дата чтения не может быть раньше даты добавления')
+            raise ValueError("Дата чтения не может быть раньше даты добавления")
+
 
 def validate_rating(rating) -> None:
     if rating:
-        validate_field('rating', rating)
+        validate_field("rating", rating)
+
 
 def validate_search_field(search_field) -> None:
-    validate_field('search_field', search_field)
+    validate_field("search_field", search_field)
+
 
 def validate_update_name(update_name, library) -> None:
-    validate_field('update_name', update_name)
+    validate_field("update_name", update_name)
     if update_name not in library.select_books():
         raise ValueError(f'Книга с названием "{update_name}" не найдена в библиотеке.')
 
+
 def validate_update_field(update_field) -> None:
-    validate_field('update_field', update_field)
+    validate_field("update_field", update_field)
+
 
 validation_functions = {
-    'name': validate_name,
-    'authors': validate_authors,
-    'genres': validate_genres,
-    'year': validate_year,
-    'width': validate_width,
-    'height': validate_height,
-    'book_type': validate_book_type,
-    'source': validate_source,
-    'date_added': validate_date_added,
-    'date_read': validate_date_read,
-    'rating': validate_rating,
-    'search_field': validate_search_field,
-    'update_name': validate_update_name,
-    'update_field': validate_update_field
+    "name": validate_name,
+    "authors": validate_authors,
+    "genres": validate_genres,
+    "year": validate_year,
+    "width": validate_width,
+    "height": validate_height,
+    "book_type": validate_book_type,
+    "source": validate_source,
+    "date_added": validate_date_added,
+    "date_read": validate_date_read,
+    "rating": validate_rating,
+    "search_field": validate_search_field,
+    "update_name": validate_update_name,
+    "update_field": validate_update_field,
 }
